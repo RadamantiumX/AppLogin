f246763236cf5879877e03c376b6eeaf65b45fa5
loadEdar <eduardo.arede@gmail.com>
2022-09-03 18:58:19 -0300

    se agrego una imagen

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,14 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est voluptatibus animi quis consequuntur quidem dignissimos doloremque aliquid adipisci enim quam, placeat deleniti saepe, ratione harum optio voluptatem soluta repellat tempora.</p></div>
+                <h3>Cuales son las ventajas de tener un sitio web propio?</h3>
+            </div>
+        </div>
+    </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <img src="https://morguefile.nyc3.cdn.digitaloceanspaces.com/pub/secretagency/images/8/large/e9574607-6972-4e68-8eb7-6ace1ecc5987.1661783077.jpg" alt="imag/work">
             </div>
         </div>
     </div>
