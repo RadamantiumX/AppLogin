fb2c4cf5a8419d58d1ec05229aea9ea2bdcc29e0
loadEdar <eduardo.arede@gmail.com>
2022-09-03 18:46:46 -0300

    Se aplico un pequeño parrafo al DASHBOARD

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est voluptatibus animi quis consequuntur quidem dignissimos doloremque aliquid adipisci enim quam, placeat deleniti saepe, ratione harum optio voluptatem soluta repellat tempora.</p></div>
             </div>
         </div>
     </div>
