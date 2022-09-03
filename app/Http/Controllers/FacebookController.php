3530f3d28efc26f0d61c787efa8ef5d0a85d1043
loadEdar <eduardo.arede@gmail.com>
2022-09-03 00:09:34 -0300

    Se añaden archivos y ficheros

---
+++ app/Http/Controllers/FacebookController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Exception;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class FacebookController extends Controller
+{
+    public function redirect()
+    {
+    return Socialite::driver('facebook')->redirect();
+    }
+
+    public function callback()
+    {
+        try
+        {
+            $user = Socialite::driver('facebook')->user();
+            $finduser = User::where('facebook_id',$user->id)->first();
+
+            if($finduser){
+                Auth::login($finduser);
+                return redirect()->intended('dashboard');
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'facebook_id'=>$user->id,
+                    'password' => encrypt('123456dummy') 
+                ]);
+
+                Auth::login($newUser);
+
+                return redirect()->intended('dashboard');
+            }
+        }catch(Exception $e){
+            dd($e->getMessage());
+        }
+        
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,65 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <x-jet-validation-errors class="mb-4" />
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div>
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Contraseña') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label for="remember_me" class="flex items-center">
+                    <x-jet-checkbox id="remember_me" name="remember" />
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Olvidaste tu contraseña?') }}
+                    </a>
+                @endif
+
+                <x-jet-button class="ml-4">
+                    {{ __('Ingresar') }}
+                </x-jet-button>
+            </div>
+            <div class="block mt-4">O Acceder con</div>
+            <!--Login GOOGLE-->
+            <div class="flex items-center justify-end mt-4">
+                <a href="{{ url('auth/google') }}">
+                    <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
+                </a>
+            </div>
+            <!--Login FACEBOOK-->
+            <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v14.0&appId=594866088789238&autoLogAppEvents=1" nonce="NBqdtYJz"></script>
+            <div class="flex items-center justify-end mt-4"> 
+            <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="true" data-use-continue-as="true">
+                <a href="{{ url('auth/facebook') }}" class="btn btn-primary">Ingresar con FacebookS</a>   
+            </div>                         
+                             
+            </div>
+
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
