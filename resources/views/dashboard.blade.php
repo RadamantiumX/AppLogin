e9112d8e2e0b17cdba5532f8235dab441146750e
loadEdar <eduardo.arede@gmail.com>
2022-09-03 19:26:59 -0300

    Se agrego una lista no ordenada y un subtitulo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,15 +8,30 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h3>Cuales son las ventajas de tener un sitio web propio?</h3>
+                <h2>Cuales son las ventajas de tener un sitio web propio?</h2>
+            </div>
+        </div>
+    </div>
+    <div class=>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                
+                <img id="img" src="https://morguefile.nyc3.cdn.digitaloceanspaces.com/pub/secretagency/images/8/large/e9574607-6972-4e68-8eb7-6ace1ecc5987.1661783077.jpg" alt="imag/work">
             </div>
         </div>
     </div>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <img src="https://morguefile.nyc3.cdn.digitaloceanspaces.com/pub/secretagency/images/8/large/e9574607-6972-4e68-8eb7-6ace1ecc5987.1661783077.jpg" alt="imag/work">
-            </div>
-        </div>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <ul>
+            <li>*Ofrece una aspecto mas prefesional sobre tu emprendimiento</li>
+            <li>*Mejoran tus posibilidades de llegar al publico deseado</li>
+            <li>*Podes administrar tu propia marca</li>
+            <li>*Contacto constante con tus clientes</li>
+            <li>*Visualizar múltiples contenidos</li>   
+        </ul>
+        <h3>Esto es solo el comienzo</h3>
+       </div> 
+    </div>
     </div>
 </x-app-layout>
