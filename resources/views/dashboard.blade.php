ce444b4b4d0ffa27c5f047b684d9afa1410efab9
loadEdar <eduardo.arede@gmail.com>
2022-09-03 19:45:21 -0300

    Se agrego un formulario

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,7 +30,27 @@
             <li>*Contacto constante con tus clientes</li>
             <li>*Visualizar múltiples contenidos</li>   
         </ul>
-        <h3>Esto es solo el comienzo</h3>
+        <h3>Esto es solo el comienzo, podes empezar ya mismo... Nosotros nos encargamos de todo.</h3>
+       </div> 
+    </div>
+    </div>
+    <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          <button>Necesitas asesorarte?</button>
+       </div> 
+    </div>
+    </div>
+    <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <h3>Dejanos los detalles, nos vamos a estar comunicando con vos para ofrecerte todas las opciones que mas te convengan</h3>
+          <form>
+            <input type="text">
+            <input type="text">
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <input type="submit">
+          </form>
        </div> 
     </div>
     </div>
