342229b884f89000b296b6362ddf4cbdb41c365d
loadEdar <eduardo.arede@gmail.com>
2022-09-03 21:51:52 -0300

    Se completaron los campos de la tabla "Comentarios"

--- database/migrations/2022_09_03_230710_create_comentarios_table.php
+++ database/migrations/2022_09_03_230710_create_comentarios_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('comentarios', function (Blueprint $table) {
             $table->id();
+            $table->string('email');
+            $table->string('celular');
+            $table->string('comentario');
             $table->timestamps();
         });
     }
