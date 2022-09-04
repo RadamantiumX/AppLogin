08c65076d5c5a9863b799d24df25e3b5c44e065f
loadEdar <eduardo.arede@gmail.com>
2022-09-03 23:46:40 -0300

    Se agregaron atributos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <h3>Dejanos los detalles, nos vamos a estar comunicando con vos para ofrecerte todas las opciones que mas te convengan</h3>
-          <form>
+          <form action="" method="POST">
             @csrf
             <label for="">Email</label>
             <input type="email" required max="50" value="{{ Auth::user()->email }}">
