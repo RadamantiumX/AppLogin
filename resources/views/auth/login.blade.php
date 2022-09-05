43fad93cf074894f859dcef1ea18fcca05d63463
loadEdar <eduardo.arede@gmail.com>
2022-09-04 23:27:32 -0300

    NAda

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
                     <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
                 </a>
             </div>
-            
+           
                                      
                              
            
